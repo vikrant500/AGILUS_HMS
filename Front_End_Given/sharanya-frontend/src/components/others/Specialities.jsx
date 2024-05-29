51ce4ae7248bd8e19c1746ee06649f4023443b61
Kss004 <166715598+Kss004@users.noreply.github.com>
2024-05-30 03:42:39 +0530

    hover and css
    hover and css

--- Front_End_Given/sharanya-frontend/src/components/others/Specialities.jsx
+++ Front_End_Given/sharanya-frontend/src/components/others/Specialities.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Specialities = ({ specialities }) => {
   return (
     <div className="bg-white text-black  py-10 flex flex-col gap-4 ">
-      <h3 className="text-2xl text-center font-bold">Our Specialities</h3>
+      <h3 className="text-2xl text-center font-bold">Our Specilities</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 md:px-8 lg:px-16">
         {specialities.map((item, i) => (
           <div key={i} className="bg-white rounded-md p-2  flex gap-2 w-full shadow-xl">
@@ -13,7 +13,7 @@ const Specialities = ({ specialities }) => {
               <div className="flex-shrink-0 w-24 h-24 overflow-hidden rounded-md bg-red-200">
                 <img
                   src={item.image}
-                  className="w-24 h-24 object-cover"
+                  className="w-50 h-50 object-cover"
                   alt="specialities"
                 />
               </div>
--- Front_End_Given/sharanya-frontend/src/pages/treatments/index.jsx
+++ Front_End_Given/sharanya-frontend/src/pages/treatments/index.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { useParams, Outlet } from "react-router-dom";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Reviews from "../home/components/Reviews";
 import Figures from "../../components/others/Figures";
 import WhyUs from "../../components/others/WhyUs";
 import FAQ from "../../components/others/FAQ";
-import { assets } from "../../assets";
 import Specialities from "../../components/others/Specialities";
-import { useParams } from "react-router-dom";
 import { examples } from "./constant/examples";
+
 const Treatments = () => {
   const [data, setData] = useState({
     faq_list: [],
@@ -17,84 +17,38 @@ const Treatments = () => {
     section_2_data: {},
   });
   const [loading, setLoading] = useState(true);
-  // const faq_list = [
-  //   {
-  //     question: "test 2",
-  //     answer: "test 2",
-  //   },
-  //   {
-  //     question: "test 3",
-  //     answer: "test 3",
-  //   },
-  // ];
-
-  // const section_1_data = {
-  //   heading: "Specialized center for Proctology Treatment",
-  //   description:
-  //     "Sharanya Care is a Multispecialty healthcare provider which aims to deliver a hassle-free surgical experience to all patients by leveraging technology, and a set of advanced operations and powerful processes. Proctology is a branch of medicine that deals with the diagnosis and treatment of diseases and medical conditions related to the anus and the rectum.",
-  // };
-
-  // const specialities = [
-  //   {
-  //     title: "Gynaecology",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, voluptatum.",
-  //   },
-  //   {
-  //     title: "Laparoscopy",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, voluptatum.",
-  //   },
-  //   {
-  //     title: "Proctology",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, voluptatum.",
-  //   },
-  // ];
-
-  // const section_2_data = {
-  //   title: "Proctology Treatment With Advanced Laser Treatment",
-  //   description:
-  //     "Proctology is the branch of medicine that deals with diagnosis, managing and treating problems related to anal, colon and rectum. It is also known as colorectal treatment. Sharanya Care is a multispeciality clinic that aims to provide best proctology treatment in India. The highly experienced Proctologists at Sharanya Care conduct minimally invasive laser treatment for problems that include hemorrhoids, anal fistula, fissure, chronic constipation and pilonidal sinus.",
-  //   list: [
-  //     "USFDA-Approved Procedure",
-  //     "No Cuts, No Wounds, No Stitches",
-  //     "Daycare Procedure",
-  //     "Quick Recovery",
-  //     "Covered By Insurance",
-  //   ],
-  // };
-
-  const { id } = useParams();
-  console.log(id);
+  const { treatmentId } = useParams();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    if (examples[id]) {
+    if (examples[treatmentId]) {
       setData({
-        faq_list: examples[id].faq_list,
-        section_1_data: examples[id].section_1_data,
-        specialities: examples[id].specialities,
-        section_2_data: examples[id].section_2_data,
+        faq_list: examples[treatmentId].faq_list,
+        section_1_data: examples[treatmentId].section_1_data,
+        specialities: examples[treatmentId].specialities,
+        section_2_data: examples[treatmentId].section_2_data,
       });
       setLoading(false);
     }
-  }, [id]);
+  }, [treatmentId]);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
   return (
     <div>
       {!loading && (
         <>
           <Section1 section_1_data={data.section_1_data} />
           <Section2 section_2_data={data.section_2_data} />
-          <Specialities specialities={data.specialities} />
+          <Specialities
+            specialities={data.specialities.map((speciality) => ({
+              ...speciality,
+              link: `/treatment/${treatmentId}/specialities/${speciality.title.toLowerCase().replace(/ /g, "-")}`,
+            }))}
+          />
           <Figures />
           <Reviews />
           <WhyUs />
           <FAQ faq_list={data.faq_list} />
+          <Outlet />
         </>
       )}
     </div>

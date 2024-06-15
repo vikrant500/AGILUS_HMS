fd2e8a8db4e6f3a5295eb1a9e101c0fac89bb28d
Rishika Bhagawati <rishikabhagawati02@gmail.com>
2024-06-16 00:29:43 +0530

    just fixed minor search bar problems

--- Vik_blog_alpha/put in client/put in src/Header.js
+++ Vik_blog_alpha/put in client/put in src/Header.js
@@ -1,74 +1,60 @@
-import { Link } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import { UserContext } from "./UserContext";
 
-export default function Header(){
-
-  const {setUserInfo,userInfo} = useContext(UserContext);
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  const location = useLocation();
 
   useEffect(() => {
-
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
-
     }).then(response => {
-
       response.json().then(userInfo => {
-
         setUserInfo(userInfo);
-
       });
-
     });
-
-  }, []);
-
+  }, [setUserInfo]);
 
   function logout() {
-
     fetch('http://localhost:4000/logout', {
-
       credentials: 'include',
       method: 'POST',
+    }).then(() => {
+      setUserInfo(null);
+      // Redirect to home page after logout
+      window.location.href = '/'; // Directly setting href for full page reload
     });
-
-    setUserInfo(null);
-
   }
 
   const username = userInfo?.username;
 
-  return(
+  const handleLogoClick = () => {
+    // Reset search query and fetch all posts
+    const queryParams = new URLSearchParams(location.search);
+    queryParams.set('q', ''); // Set search query to empty string
+    queryParams.set('page', '1'); // Reset page to 1
+    window.location.href = `/?${queryParams.toString()}`; // Full page reload
+  };
 
+  return (
     <header>
-
-      <Link to="/" className="logo">MyBlog</Link>
-
+      <Link to="/" className="logo" onClick={handleLogoClick}>
+        MyBlog
+      </Link>
       <nav>
-
-        {username && (
-
+        {username ? (
           <>
             <Link to="/create">Create new post</Link>
             <a onClick={logout}>Logout</a>
           </>
-
-        )}
-
-        {!username && (
-
+        ) : (
           <>
             <Link to="/login">Login</Link>
-
             <Link to="/register">Register</Link>
           </>
-
         )}
-
       </nav>
-
     </header>
-
-    );
-
+  );
 }
